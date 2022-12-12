8f7546b3175643ab8c995698864ca253add22805
ThalesAdriel <115805292+ThalesAdriel@users.noreply.github.com>
2022-12-12 18:35:11 +0000

    Add files via upload

--- status.sh
+++ status.sh
@@ -19,12 +19,12 @@ disk_status(){
 	read -p "Precione alguma tecla para continuar... "
 }
 conection_status(){
-	echo "Conetividade com internet:"
-	Ping1=$(ping -c 5 8.8.8.8 > /dev/null);
+	echo "Conectividade com internet..."
+	Ping1=$(ping -c 2 8.8.8.8 > /dev/null);
 	if [ $? -eq 0 ];then
-	echo -e "${Color_green} Conectado ${Color_default}"
+	echo -e "${Color_green} |Conectado| ${Color_default}"
 	else
-	echo -e "${Color_red} Desconectado ${Color_default}"
+	echo -e "${Color_red} |Desconectado| ${Color_default}"
 	fi
 	read -p "Precione alguma tecla para continuar... "
 }
@@ -35,33 +35,47 @@ programs_status(){
 }
 system_info(){
 	uptime_time=$(uptime -p);
+	uptime_time_since=$(uptime -s);
 	echo -e "O sistema está ligado á ${Color_green}$uptime_time${Color_default}"
+	echo -e "O sistema está ativo desde ${Color_green}$uptime_time_since${Color_default}"
+	read -p "Precione alguma tecla para continuar... "
+}
+ascii_test(){
+	echo -e "${Color_green}___  ___ _____  _   _  _   _ "
+	echo "|  \/  ||  ___|| \ | || | | |"
+	echo "| .  . || |__  |  \| || | | |"
+	echo "| |\/| ||  __| | .   || | | |"
+	echo "| |  | || |___ | |\  || |_| |"
+	echo -e "\_|  |_/\____/ \_| \_/ \___/${Color_default} "
 	read -p "Precione alguma tecla para continuar... "
 }
 menu(){
 	clear
 	name=$(whoami);
-	echo "Bem-vindo, $name"
+	echo "|Bem-vindo, $name"
 	#date "+Data: %D%nHora: %T"
-	echo -e "${Color_purple}+++++++++==-MENU-==+++++++++++${Color_default}"
-	echo -e "1.- ${Color_blue}Ultilização de memoria.${Color_default} "
-	echo -e "2.- ${Color_cyan}Ultilização de disco.${Color_default} "
-	echo -e "3.- ${Color_green}Conetividade com internet.${Color_default}  "
-	echo -e "4.- ${Color_yellow}Processos.${Color_default} "
-	echo -e "5.- ${Color_red}informações do sistema.${Color_default} "
-	echo -e "6.- ${Color_purple}Sair. ${Color_default}"
-	echo -e "${Color_purple}++++++++++++++++++++++++++++++${Color_default}"
+	echo -e "${Color_purple}|${Color_default}${Color_purple}=+=+=+=++==-MENU-==+=+=+=+=+=+=${Color_default}${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}1.- ${Color_blue}Ultilização de memoria.${Color_default}    ${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}2.- ${Color_cyan}Ultilização de disco.${Color_default}      ${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}3.- ${Color_green}Conectividade com internet.${Color_default}${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}4.- ${Color_yellow}Processos.${Color_default}                 ${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}5.- ${Color_red}informações do sistema.${Color_default}    ${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}6.- ${Color_purple}Sair. ${Color_default}                     ${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}7.- ${Color_blue}Ascii. ${Color_default}                    ${Color_purple}|${Color_default}"
+	echo -e "${Color_purple}|${Color_default}${Color_purple}=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=${Color_default}${Color_purple}|${Color_default}"
 }
 inputs(){
 	local choice
-	read -p "Escolha a opção: " choice
+	read -p "|Escolha a opção: " choice
+	echo -e "${Color_purple}|${Color_default}${Color_purple}=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=${Color_default}${Color_purple}|${Color_default}"
 	case $choice in
 	1) memory_status ;;
 	2) disk_status ;;
 	3) conection_status ;;
 	4) programs_status ;;
 	5) system_info ;;
-	6) exit 0 ;; 
+	6) exit 0 ;;
+	7) ascii_test ;;
 	*) echo "Resposta invalida"
 		read -p "Precione alguma tecla para continuar... "
 	esac
